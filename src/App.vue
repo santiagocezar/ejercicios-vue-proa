67b183d1acfac9e31114cee128e1f1bf8a806717
Santiago Cézar <santiagocezar2013@gmail.com>
2021-09-07 17:26:24 -0300

    commit: initial

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Ejercicio1 from "./components/Ejercicio1.vue";
+
+</script>
+
+<template>
+    <Ejercicio1 />
+</template>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+html,
+body,
+#app {
+    background-color: aliceblue;
+    /*background-image: url("/assets/caprincho.jpg");
+    background-size: cover;
+    background-position: center;*/
+    color: #020103;
+    height: 100%;
+}
+</style>
---
+++ src/components/Ejercicio1.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const nombre = ref('')
+</script>
+
+<template>
+    <div class="root">
+        <input autofocus v-model="nombre" type="text" />
+        <p class="hola">Hola, {{ nombre || '...' }}</p>
+    </div>
+</template>
+
+<style scoped>
+.root {
+    font-family: "Poppins", sans-serif;
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+input {
+    background-color: white;
+    border: none;
+    font: inherit;
+    outline: none;
+    padding: 8px;
+    border: 2px solid black;
+    box-shadow: 4px 4px 0 lightcoral;
+    margin-bottom: 8px;
+}
+.hola {
+    font-size: 56px;
+    font-weight: 300;
+    text-align: center;
+}
+</style>
